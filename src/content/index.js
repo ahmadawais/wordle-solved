8fce6d48f1598b715bd388d07bf3e9c32c8ed750
Ahmad Awais <me@AhmadAwais.com>
2022-01-26 19:18:27 +0500

    🐛 FIX: No consoles

--- src/content/index.js
+++ src/content/index.js
@@ -1,10 +1,9 @@
-console.log('content script')
-const solution = JSON.parse(window.localStorage.gameState).solution
+const solution = JSON.parse(window.localStorage.gameState).solution;
 
-solution.split('').forEach((letter) => {
-  document
-    .querySelector('body > game-app')
-    .shadowRoot.querySelector('#game > game-keyboard')
-    .shadowRoot.querySelector(`button[data-key="${letter}"]`)
-    .click()
-})
+solution.split('').forEach(letter => {
+	document
+		.querySelector('body > game-app')
+		.shadowRoot.querySelector('#game > game-keyboard')
+		.shadowRoot.querySelector(`button[data-key="${letter}"]`)
+		.click();
+});
