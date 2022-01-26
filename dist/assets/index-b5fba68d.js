5f3b5baf188704ef432ead9a0409f758acd35482
Ahmad Awais <me@AhmadAwais.com>
2022-01-26 20:01:09 +0500

    🐛 FIX: Sync .gitignore

--- dist/assets/index-b5fba68d.js
+++
@@ -1,8 +0,0 @@
-(function () {
-	'use strict';
-
-	const importPath = /*@__PURE__*/JSON.parse('"../content/index.js"');
-
-	import(chrome.runtime.getURL(importPath));
-
-}());
--- dist/content/index.js
+++
@@ -1,4 +0,0 @@
-const solution = JSON.parse(window.localStorage.gameState).solution;
-solution.split('').forEach(letter => {
-  document.querySelector('body > game-app').shadowRoot.querySelector('#game > game-keyboard').shadowRoot.querySelector(`button[data-key="${letter}"]`).click();
-});
